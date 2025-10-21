f7878df31b53900777f5133c12fcdfd6dc63bb7c
frodnie838 <frodnie838@g.educaand.es>
2025-10-21 14:34:43 +0200

    subida P2 UD3

---
+++ UD3/P2/Ej6/Ej6/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ej6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BotonM(object sender, MouseEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                MessageBox.Show("Contenido: "+btn.Content+" es un botón verde");
+            }
+        }
+    }
+}
---
+++ UD3/P2/Ej7/Ej7/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ej7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BClick(object sender, RoutedEventArgs e)
+        {
+            ShowSelectedText();
+        }
+
+        private void ImagenClick(object sender, MouseButtonEventArgs e)
+        {
+            ShowSelectedText();
+            e.Handled = true;
+        }
+
+        private void ShowSelectedText()
+        {
+            var seleccionado = tBox?.SelectedText;
+            if (string.IsNullOrEmpty(seleccionado))
+            {
+                MessageBox.Show("No hay selección en el TextBox.", "Selección", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Has seleccionado: "+seleccionado, "Selección", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
