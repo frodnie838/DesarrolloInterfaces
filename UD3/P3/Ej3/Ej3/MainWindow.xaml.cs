7fa66ccd4b311e9ca512bd5c20ab7be824767a76
frodnie838 <frodnie838@g.educaand.es>
2025-10-27 11:08:16 +0100

    subida 27/10

---
+++ UD3/P3/Ej3/Ej3/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ej3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        const string usuario = "Admin";
+        const string password = "1234";
+        public void Password(Object sender, RoutedEventArgs e)
+        {
+            String usu = txtUsu.Text;
+            String pass = pwdPass.Password;
+
+            if (usu.Equals(usuario) && pass.Equals(password)) {
+                MessageBox.Show("Bienvenido, "+usuario+"!!");
+                
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta.");
+                pwdPass.Clear();
+            }
+
+        }
+    }
+}
---
+++ UD3/P3/Ej4/Ej4/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ej4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private bool contador = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void CambiarImg(object sender, RoutedEventArgs e)
+        {
+            if (contador)
+            {
+                Imagen.Source = new BitmapImage(new Uri("/images/andalucia.png", UriKind.Relative));
+                contador = false;
+            }
+            else
+            {
+                Imagen.Source = new BitmapImage(new Uri("/images/dragon.png", UriKind.Relative));
+                contador = true;
+            }
+        }
+    }
+}
