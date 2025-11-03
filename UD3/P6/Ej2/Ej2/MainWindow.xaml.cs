6aabd7e662f31be9d21a6277dea9d50b1a4dce7d
frodnie838 <frodnie838@g.educaand.es>
2025-11-03 11:13:25 +0100

    p6

---
+++ UD3/P6/Ej2/Ej2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ej2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CutCommand_CanExecute (object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (txtEditar != null) && (txtEditar.SelectionLength > 0);
+        }
+        private void CutCommand_Executed (object sender, ExecutedRoutedEventArgs e)
+        {
+            txtEditar.Cut();
+        }
+        private void PasteCommand_CanExecute (Object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = Clipboard.ContainsText();
+        }
+        private void PasteCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtEditar.Paste();
+        }
+    }
+}
---
+++ UD3/P6/Ej3/Ej3/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ej3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Enviar(object sender, RoutedEventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string email = txtEmail.Text;
+            string comentario = txtComentario.Text;
+            string mensaje = "El usuario " + nombre + " con email " + email + " envía el mensaje " + comentario;
+            if (string.IsNullOrEmpty(comentario) )
+            {
+                mensaje = "El usuario " + nombre + " con email " + email + " no ha enviado ningún mensaje ";
+            }
+
+            
+
+            MessageBox.Show(mensaje);
+        }
+        
+    }
+}
