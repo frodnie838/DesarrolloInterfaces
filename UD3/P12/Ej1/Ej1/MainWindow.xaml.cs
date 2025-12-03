e9452128805bcb55773ddca23339cbc3e30f7c3f
frodnie838 <frodnie838@g.educaand.es>
2025-12-03 14:42:17 +0100

    p12

---
+++ UD3/P12/Ej1/Ej1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ej1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabCambio(object sender, SelectionChangedEventArgs e)
+        {
+            if (TabMain.SelectedIndex == 1)
+            {
+                double v1, v2;
+                if (!double.TryParse(tb1.Text, out v1))
+                {
+                    MessageBox.Show("El primer valor no es válido.", "Error",MessageBoxButton.OK, MessageBoxImage.Warning);
+                    TabMain.SelectedIndex = 0;
+                    return;
+                }
+                if (!double.TryParse(tb2.Text, out v2))
+                {
+                    MessageBox.Show("El segundo valor no es válido.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    TabMain.SelectedIndex = 0;
+                    return;
+                }
+                tbResult.Text = v1+" x "+v2+" = "+(v1 * v2).ToString();
+            }
+        }
+    }
+}
---
+++ UD3/P12/Ej2/Ej2/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ej2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string connectionString = "Server=localhost; port=3306; Database=prueba; Uid=root; Pwd=password";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MostrarD(object sender, RoutedEventArgs e)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM users";
+                    MySqlCommand cmd = new MySqlCommand(sql, connection);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    StringBuilder sb = new StringBuilder();
+                    Datos d = new Datos(reader, sb);
+                    d.ShowDialog();
+                }
+                catch (MySqlException ex)
+                {
+                    tbResult.Text = "Error al conectar con la Base de Datos. " + ex.Message;
+                }
+            }
+
+            }
+
+        private void BBDD(object sender, RoutedEventArgs e)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    tbResult.Text = "Conexión establecida.";
+                }
+                catch (MySqlException ex)
+                {
+                    tbResult.Text = "Error al conectar con la Base de Datos. "+ex.Message;
+                }
+            }
+        }
+    }
+}
